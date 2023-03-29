33a9869f068879ffc063c44913b571bc0755cfbe
sanaf1333 <sanaf1333@gmail.com>
2023-03-29 19:55:25 +0500

    moved defaultProps to interfaces

---
+++ src/interfaces/card-style-default-props.ts
@@ -0,0 +1,12 @@
+import { dateCardProps } from "./date-card-props-interface";
+
+export const defaultProps: dateCardProps = {
+    headerColor: '#eb4c34',
+    monthColor: 'white',
+    dayColor: 'gray',
+    dateColor: 'gray',
+    cardBackgroundColor: 'white',
+    cardWidth: 150,
+    disabledColor: 'gray',
+    selectedBorder: '1px solid gray',
+};
